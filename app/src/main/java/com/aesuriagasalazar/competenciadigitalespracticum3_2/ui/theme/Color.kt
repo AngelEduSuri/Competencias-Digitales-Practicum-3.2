cfcdcdf3cf281f9ec35271f2e34845c0cc967035
AngelEduSuri <angelsuriaga@hotmail.com>
2022-06-09 13:57:07 -0500

    Commit inicial y configuracion del proyecto

---
+++ app/src/main/java/com/aesuriagasalazar/competenciadigitalespracticum3_2/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.aesuriagasalazar.competenciadigitalespracticum3_2.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryColor = Color(0xFF1565c0)
+val PrimaryDarkColor = Color(0xFF003c8f)
+val SecondaryColor = Color(0xFFd4e157)
+val SecondaryDarkColor = Color(0xFFa0af22)
+val OnPrimary = Color(0xFFffffff)
+val OnSecondary = Color(0xFF000000)
